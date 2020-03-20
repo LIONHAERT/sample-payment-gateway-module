df01966e6dc5d67ca80b85f6316b37dd8ff61796
WISECP <50171491+wisecp@users.noreply.github.com>
2020-03-20 19:03:36 +0300

    Update tr.php

--- coremio/modules/Payment/ExamplePaymentGateway/lang/tr.php
+++ coremio/modules/Payment/ExamplePaymentGateway/lang/tr.php
@@ -10,6 +10,9 @@ return [
 
     'field1-desc'           => 'Alan 1 için Açıklama',
     'field2-desc'           => 'Alan 2 için Açıklama',
+    
+     'commission-rate'      => "Komisyon Oran\xc4\xb1 (%)",
+    'commission-rate-desc' => "Komisyon \xc3\xbccreti yans\xc4\xb1tmak % de\xc4\x9fer giriniz. (Ge\xc3\xa7ersiz k\xc4\xb1lmak i\xc3\xa7in bo\xc5\x9f b\xc4\xb1rak\xc4\xb1n\xc4\xb1z.) ",
 
     'save-button'          => "Ayarlar\xc4\xb1 Kaydet",
     'success1'             => "Ayarlar Ba\xc5\x9far\xc4\xb1yla Kaydedildi.",
